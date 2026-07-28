eb44a99de7c038adec2aca387053e9a2ae0afddf
jisoo <0801ljs@naver.com>
2026-07-28 20:03:58 +0900

    fix: React 웹폰트 로딩 방식 수정

--- react-frontend/src/hooks/usePageStyles.js
+++ react-frontend/src/hooks/usePageStyles.js
@@ -1,11 +1,13 @@
 import { useInsertionEffect } from 'react';
 import themeStyles from '../../../styles/bugdex-theme.css?inline';
 
+const themeStylesWithoutFontImport = themeStyles.replace(/^@import[^\n]*\n+/, '');
+
 export function usePageStyles(pageName, pageStyles) {
   useInsertionEffect(() => {
     const element = document.createElement('style');
     element.dataset.pageStyles = pageName;
-    element.textContent = `${pageStyles}\n${themeStyles}`;
+    element.textContent = `${pageStyles}\n${themeStylesWithoutFontImport}`;
     document.head.appendChild(element);
 
     return () => element.remove();
